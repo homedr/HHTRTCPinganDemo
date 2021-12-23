f3b56a3c3b5504de7757e8730270f0872a00401b
liumingming <mingmingsuper@gmail.com>
2021-12-23 14:45:30 +0800

    fix

--- app/src/main/java/com/hhmedic/demo/trtc/pingan/CallAct.kt
+++ app/src/main/java/com/hhmedic/demo/trtc/pingan/CallAct.kt
@@ -2,7 +2,6 @@ package com.hhmedic.demo.trtc.pingan
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.google.gson.Gson
@@ -10,9 +9,6 @@ import com.hhmedic.android.sdk.HHDoctor
 import com.hhmedic.android.sdk.listener.HHCallExListener
 import com.hhmedic.android.sdk.model.HHCallInfo
 import com.hhmedic.demo.trtc.pingan.databinding.CallLayoutBinding
-import com.tencent.imsdk.v2.V2TIMManager
-import com.tencent.imsdk.v2.V2TIMMessage
-import com.tencent.imsdk.v2.V2TIMValueCallback
 
 class CallAct : BaseAct() {
 
@@ -20,7 +16,7 @@ class CallAct : BaseAct() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this,R.layout.call_layout)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.call_layout)
         initUI()
     }
 
@@ -28,7 +24,7 @@ class CallAct : BaseAct() {
         initActionBar(mBinding.toolbar)
         setExtension()
         mBinding.callAdult.setOnClickListener {
-            HHDoctor.callForAdult(this,object : HHCallExListener {
+            HHDoctor.callForAdult(this, object : HHCallExListener {
 
                 override fun onFinish(time: Long) {
                 }
@@ -71,7 +67,7 @@ class CallAct : BaseAct() {
 
         mBinding.callChild.setOnClickListener {
             setExtension()
-            HHDoctor.callForChild(this,object : HHCallExListener {
+            HHDoctor.callForChild(this, object : HHCallExListener {
 
 
                 override fun onFinish(time: Long) {
@@ -116,10 +112,10 @@ class CallAct : BaseAct() {
         mBinding.startCallUserToken.setOnClickListener {
             val userToken = mBinding.callUserToken.text.toString()
             if (TextUtils.isEmpty(userToken)) {
-                Toast.makeText(this,"请输入需要呼叫的userToken",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "请输入需要呼叫的userToken", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            HHDoctor.call(this,userToken,object : HHCallExListener {
+            HHDoctor.call(this, userToken, object : HHCallExListener {
 
                 override fun onFinish(time: Long) {
                 }
@@ -168,20 +164,26 @@ class CallAct : BaseAct() {
 
     private fun sendMessage() {
 
-        val user = User()
-        user.age = 18
-        user.name = "Test"
-        val json = Gson().toJson(user)
-        V2TIMManager.getInstance().sendC2CCustomMessage(json.toByteArray(Charsets.UTF_8),"100839328", object : V2TIMValueCallback<V2TIMMessage> {
-            override fun onSuccess(t: V2TIMMessage?) {
-                Log.e("send message","send message success")
-            }
+//        val user = User()
+//        user.age = 18
+//        user.name = "Test"
+//        val json = Gson().toJson(user)
+//        V2TIMManager.getInstance().sendC2CCustomMessage(json.toByteArray(Charsets.UTF_8),"100839328", object : V2TIMValueCallback<V2TIMMessage> {
+//            override fun onSuccess(t: V2TIMMessage?) {
+//                Log.e("send message","send message success")
+//            }
+//
+//            override fun onError(code: Int, desc: String?) {
+//                Log.e("send message","send message failed")
+//            }
+//
+//        })
 
-            override fun onError(code: Int, desc: String?) {
-                Log.e("send message","send message failed")
-            }
+        val remoteMessage = TestRemoteMessage()
 
-        })
+        val message = Gson().toJson(remoteMessage)
+
+        HHDoctor.remoteMessage(this, message, null)
     }
 
     private fun setExtension() {
---
+++ app/src/main/java/com/hhmedic/demo/trtc/pingan/TestRemoteMessage.java
@@ -0,0 +1,14 @@
+package com.hhmedic.demo.trtc.pingan;
+
+import java.io.Serializable;
+
+public class TestRemoteMessage implements Serializable {
+
+    public String command = "call_back";
+
+    public String orderId = "F2021122314191666506";
+
+    public String userToken = "830256BC2CD1970249F87BF35FED13783F0D04F68EA2608F6783B874E4F50EEF";
+
+    public long  doctorUuid = 100017304;
+}
